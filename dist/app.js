6123899a624779594c6afc28b99f2cc52b9d44c8
miliincode <milena0202@icloud.com>
2024-06-13 17:09:38 +0300

    new functions for pagination

--- dist/app.js
+++ dist/app.js
@@ -1,10 +1,13 @@
 import { data } from "./data/data.js";
 const cards = document.querySelector(".cards");
 const input = document.querySelector("#input");
+const pagination = document.querySelector(".pagination");
+const notesOnPage = 12; // Колличество отображаемых карточек в одной странице
 // ↓ Функция по отображению карточек
 function showCards(data) {
     cards.innerHTML = "";
-    data.forEach((item) => cards.append(createCard(item)));
+    // Отображаение первых элементов массива
+    data.slice(0, notesOnPage).forEach((item) => cards.append(createCard(item)));
 }
 showCards(data);
 // ↓ Функция по созданию карточки
@@ -24,6 +27,36 @@ function createUniqueWords(string) {
         .split(" ")
         .filter((word) => word);
     return new Set(unique);
+}
+let items = [];
+let counOfItems = Math.ceil(data.length / notesOnPage); // Колличество элементов нумерации страниц 
+// ↓ Цикл по добавлению ячеек в секцию с пагинацией ...
+for (let i = 1; i <= counOfItems; i++) {
+    let item = document.createElement("div");
+    item.classList.add("pagination__item");
+    item.innerHTML = i.toString(); // Число становится текстом
+    pagination.appendChild(item);
+    items.push(item); // ... и добавление элементов в пустой массив items
+}
+// ↓ Цикл для каждого элемента массива с событием
+for (let item of items) {
+    item.addEventListener("click", pagesCalculation);
+}
+// Функция по расчету страниц для раздела с пагинацией
+function pagesCalculation() {
+    // ↓ This ссылается на содержимое элемента
+    let pageNum = +this.innerHTML;
+    let start = (pageNum - 1) * notesOnPage;
+    let end = start + notesOnPage;
+    let notes = data.slice(start, end);
+    cards.innerHTML = "";
+    for (let note of notes) {
+        createItems(note);
+    }
+}
+// ↓ Функция по отображению карточек по нажатию на ячейки
+function createItems(text) {
+    cards.append(createCard(text));
 }
 // ↓ Событие по поиску введеного названия
 input.addEventListener("input", (event) => {
